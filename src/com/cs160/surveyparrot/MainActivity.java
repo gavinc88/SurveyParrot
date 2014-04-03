59c7657597d3b462a6c14187e535fb10f9bfbc15
Gavin Chu <gavincca@gmail.com>
2014-04-02 19:45:56 -0700

    update

--- src/com/cs160/surveyparrot/MainActivity.java
+++ src/com/cs160/surveyparrot/MainActivity.java
@@ -1,17 +1,12 @@
 package com.cs160.surveyparrot;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.os.Build;
 
 public class MainActivity extends Activity implements OnClickListener {
 
